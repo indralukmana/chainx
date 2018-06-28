35730e1996cb67e06c84cd43b042651616fffd59
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-06-28 10:43:08 +0700

    Fill in the plugin informations

---
+++ chain.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name:  Chainx
+Description:  Don't break the posting chain. Visualize your daily posting in calendar views. Motivate yourself to post daily.
+Plugin URI:   https://www.indralukmana.com
+Author:       Indra Lukmana
+Version:      1.0
+Text Domain:  chainx
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
