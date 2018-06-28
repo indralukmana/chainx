6dde7f03a84b27d49e050aaffedbf1a789d0ba3b
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-06-28 12:22:25 +0700

    Add top level menu

--- chainx.php
+++ chainx.php
@@ -18,3 +18,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 
 }
+
+// add top-level  menu
+function chainx_add_toplevel_menu() {
+	
+	$chainx_calendar_page = add_menu_page(
+								esc_html__('Chainx Calendar', 'chainx'),
+								esc_html__('Chainx', 'chainx'),
+								'publish_posts',
+								'chainx',
+								'chainx_display_calendar',
+								'dashicons-admin-generic',
+								null
+							);
+	
+}
+add_action( 'admin_menu', 'chainx_add_toplevel_menu' );
+
+// display the plugin settings page
+function chainx_display_calendar() {
+	
+	// check if user is allowed access
+	if ( ! current_user_can( 'publish_posts' ) ) return;
+	
+	?>
+	
+
+	<div class="wrap">
+
+	<div id="calendar"> </div>
+
+	</div>
+	
+	<?php
+	
+}
+
+
